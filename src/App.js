65c80c792f0c87454ae11c078960817cfbc4dbcf
joaquin <joacodicio@gmail.com>
2022-10-08 20:11:49 +0200

    feed header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Sidebar from "./components/Sidebar/Sidebar";
+import Feed from './components/Feed/Feed.jsx'
+import Widgets from './components/Widgets/Widgets.jsx'
 
 function App() {
   return (
-    <>
+    <div className='app'>
     {/* sidebar */}
     <Sidebar/>
     {/* feed scrolleable */}
-    
+    <Feed/>
     {/* Widgets */}
-    </>
+    <Widgets/>
+    </div>
   );
 }
 
---
+++ src/components/TweetBox/TweetBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TweetBox.css'
+
+export default function TweetBox() {
+  return (
+    <div className='tweetBox'>
+      <form action="">
+        
+      </form>
+    </div>
+  )
+}
