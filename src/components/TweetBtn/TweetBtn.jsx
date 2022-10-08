210fb81f8f851f8f66f13459822003343c826469
joaquin <joacodicio@gmail.com>
2022-10-08 15:44:59 +0200

    sidebar finished

---
+++ src/components/TweetBtn/TweetBtn.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TweetBtn.css'
+
+export default function TweetBtn() {
+  return (
+    <button className='w-100 tweet-btn'>
+        Twittear
+    </button>
+  )
+}
