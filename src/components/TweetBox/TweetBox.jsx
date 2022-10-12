bf12128b151de708c206956f670269902f355558
joaquin <joacodicio@gmail.com>
2022-10-12 22:50:41 +0200

    twitter con feed sin wg

--- src/components/TweetBox/TweetBox.jsx
+++ src/components/TweetBox/TweetBox.jsx
@@ -3,9 +3,15 @@ import './TweetBox.css'
 
 export default function TweetBox() {
   return (
-    <div className='tweetBox'>
-      <form action="">
-        
+    <div className='tweetbox'>
+      <form className='row' action="">
+        <div className='d-flex justify-items-center'>
+        <img className='tweetbox__user-img' src='https://pbs.twimg.com/profile_images/1364057247186362369/acSYBHZI_400x400.jpg'/>
+        <input className='tweetbox__input col-10' type="text" placeholder="What's happening?" />
+        </div>
+        <div className="tweetbox__actions">
+
+        </div>
       </form>
     </div>
   )
