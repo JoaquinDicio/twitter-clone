15f4265c5638cda7708ac86e8829be56487364ed
joaquin <joacodicio@gmail.com>
2022-10-15 16:58:36 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <>
+    <div className="app">
     {/* sidebar */}
     <Sidebar/>
     {/* feed scrolleable */}
     
     {/* Widgets */}
-    </>
+    </div>
   );
 }
 
